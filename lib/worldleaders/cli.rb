2bd319f640a56a7c4b27d2a0ce25e9170feb515b
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2019-02-27 19:00:51 +0000

    first commit

---
+++ lib/worldleaders/cli.rb
@@ -0,0 +1,8 @@
+require "worldleaders/cli/version"
+
+module Worldleaders
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/worldleaders/cli/version.rb
@@ -0,0 +1,5 @@
+module Worldleaders
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
