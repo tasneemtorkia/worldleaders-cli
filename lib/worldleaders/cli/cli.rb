511ee23b40d7fd76f03d63516956443aee46a7bc
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2019-03-01 18:49:22 +0000

    modified again

--- lib/worldleaders/cli/cli.rb
+++ lib/worldleaders/cli/cli.rb
@@ -1,2 +1,14 @@
-require_relative "./worldleaders/cli/version"
-require_relative "./worldleaders/cli"
+class WorldLeaders::CLI 
+  
+  def call 
+    puts "Welcome to Wolrd leaders! Which country would you like to enquire about?"
+    countries 
+  end 
+  
+  def countries 
+    puts <<--HEREDOC 
+    1.UK 
+    2.EGYPT 
+    HEREDOC 
+  end 
+  end 
--- lib/worldleaders/worldleaders.rb
+++ lib/worldleaders/worldleaders.rb
@@ -1,14 +1,2 @@
-class WorldLeaders::CLI 
-  
-  def call 
-    puts "Welcome to Wolrd leaders! Which country would you like to enquire about?"
-    countries 
-  end 
-  
-  def countries 
-    puts <<--HEREDOC 
-    1.UK 
-    2.EGYPT 
-    HEREDOC 
-  end 
-  end 
+require_relative "./worldleaders/cli/version"
+require_relative "./worldleaders/cli"
